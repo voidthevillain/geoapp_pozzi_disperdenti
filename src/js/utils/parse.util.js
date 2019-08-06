de5f266d0b6f5e5b62fed51a572711d0d33b94f7
Mihai Filip <voidthevillain@gmail.com>
2019-08-06 12:33:12 +0300

    add parse utility module

---
+++ src/js/utils/parse.util.js
@@ -0,0 +1,10 @@
+/**
+ * Parses a string to Float
+ * @param {string} value string to parse
+ * @returns {number} parsed value
+ */
+function parse(value) {
+  return parseFloat(value)
+}
+
+exports.parse = parse
