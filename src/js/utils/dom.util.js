07c777630b27cdab6fe2e61fa96e86b91b78e7de
Mihai Filip <voidthevillain@gmail.com>
2019-08-06 12:24:08 +0300

    add dom utility and grab inputs

---
+++ src/js/utils/dom.util.js
@@ -0,0 +1,48 @@
+// Buttons
+const btn_new = document.querySelector('#btn-new')
+const btn_open = document.querySelector('#btn-open')
+const btn_save = document.querySelector('#btn-save')
+const btn_print = document.querySelector('#btn-print')
+const btn_help = document.querySelector('#btn-help')
+
+const btn_minus = document.querySelector('#btn-minus')
+const btn_plus = document.querySelector('#btn-plus')
+
+const btn_calculate = document.querySelector('#btn-calculate')
+
+// File
+const input_file = document.querySelector('#input-file')
+
+// Inputs
+const input_k = document.querySelector('#k')
+const input_d = document.querySelector('#d')
+const input_n = document.querySelector('#n')
+const input_as = document.querySelector('#as')
+const input_phi = document.querySelector('#phi')
+const input_tc = document.querySelector('#tc')
+const input_dp = document.querySelector('#dp')
+const input_dt = document.querySelector('#dt')
+const input_a = document.querySelector('#a')
+
+const inputs = document.querySelectorAll('.input-value')
+
+// Outputs
+const output_hwmax = document.querySelector('#hwmax')
+
+// Table
+const table = document.querySelector('#table')
+
+// Modals
+// Save
+const btn_save_modal = document.querySelector('#btn-save-modal')
+const btn_close_save_modal = document.querySelector('#btn-close-save-modal')
+const btn_confirm_save_modal = document.querySelector('#btn-confirm-save-modal')
+const btn_ext_txt = document.querySelector('#btn-ext-txt')
+const btn_ext_gap = document.querySelector('#btn-ext-gap')
+const file_name = document.querySelector('#file-name')
+const file_ext = document.querySelector('#file-ext')
+
+// Alert
+const btn_toggle_alert = document.querySelector('#btn-toggle-alert')
+const btn_go_to_field = document.querySelector('#btn-go-to-field')
+const alert_body = document.querySelector('#alert-modal-body')
