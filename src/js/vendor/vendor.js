103615891dbe3adc7be2ef389103ac32a44f8f83
Mihai Filip <voidthevillain@gmail.com>
2019-08-06 11:48:28 +0300

    add vendor module

---
+++ src/js/vendor/vendor.js
@@ -0,0 +1,18 @@
+import '@fortawesome/fontawesome-free/js/fontawesome.min.js'
+import '@fortawesome/fontawesome-free/js/solid'
+import '../../css/bootstrap.min.css'
+import '../../css/normalize.css'
+import '../../css/main.css'
+
+// Overlay (spinner)
+const overlay = document.querySelector('#overlay')
+window.addEventListener('load', function () {
+  setTimeout(() => {
+    overlay.parentNode.removeChild(overlay)
+  }, 500)
+})
+
+// Tooltip
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
